6018d21a808e7b50a5ee491383f1ea0b4e9fe955
vh-Vu <huynh-vu@outlook.com>
2025-04-15 04:56:37 +0700

    update view for products page

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -2,17 +2,36 @@ const controller = {};
 const models = require("../models");
 const Products = models.Product;
 const Categories = models.Category;
+const Brands = models.Brand;
+const Tags = models.Tag;
 
-controller.showProductPage = async(req,res) =>{
+controller.init = async(req,res,next) =>{
+
     res.locals.categories = await Categories.findAll(
         {
         include: [{model: Products, attributes : ["id"]}]
     });
-    console.log(res.locals.categories);
+
+    res.locals.brands = await Brands.findAll({
+        include: [{model: Products, attributes : ["id"]}]
+    });
+
+    res.locals.tags = await Tags.findAll();
+
+    next();
+}
+
+controller.showProductPage = async(req,res) =>{
+    
+
     res.locals.products = await Products.findAll({
         attributes: ["id","name","imagePath","price","oldPrice","stars"]
     })
     res.render("product-list");
+}
+
+controller.showDetailPage = async(req,res) => {
+    res.render("product-detail");
 }
 
 module.exports = controller;
--- routers/productsRouter.js
+++ routers/productsRouter.js
@@ -3,5 +3,6 @@ const router = EXPRESS.Router();
 const controller = require("../controllers/productsController")
 
 router.get("/", controller.showProductPage);
+router.get("/:id", controller.showDetailPage);
 
 module.exports = router;
